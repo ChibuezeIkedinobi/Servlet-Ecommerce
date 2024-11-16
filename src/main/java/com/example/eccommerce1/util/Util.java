241b1ce832ef99b26d47c5fa7adb23e2f8a5355c
Ikedinobi <Ikdinobi@gmail.com>
2024-11-17 00:36:14 +0100

    ecommerce1

--- src/main/java/com/example/eccommerce1/util/Util.java
+++ src/main/java/com/example/eccommerce1/util/Util.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/ecommerce1";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         try {Class.forName("com.mysql.cj.jdbc.Driver");
